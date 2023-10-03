10f04693b2de1479d790ac96909b52a52c1bde96
zubairhabib <zubairhabibawan88@gmail.com>
2023-10-03 21:09:49 +0500

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">hy</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
